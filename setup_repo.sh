309e9d38f38f108d40c7a3ca46842890cbf560a0
ASmac <abdullah@teamescape.de>
2022-03-30 15:52:14 +0200

    change dir to Electroscape

--- setup_repo.sh
+++ setup_repo.sh
@@ -15,8 +15,8 @@ else
 fi
 
 cd ~
-mkdir TE
-cd TE
+mkdir Electroscape
+cd Electroscape
 
 CNTX={users}; NAME={Electroscape}; PAGE=1
 curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
@@ -24,7 +24,7 @@ curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100" |
   cut -d \" -f 4 |
   xargs -L1 git clone
 
-for f in ~/TE/*
+for f in ~/Electroscape/*
 do
     echo "Processing $f"
     cd $f
